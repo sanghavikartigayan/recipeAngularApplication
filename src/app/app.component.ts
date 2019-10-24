f2c9e679f67b4495fc3c05fbf5403ded9989faae
sanghavi-kartigayan <sanghavi.kartigayan@performance.ca>
2019-10-24 15:55:40 -0400

    added navigation between recipe and shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  loadedFeature = 'recipe';
   title = 'RecipeAngularApplication';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
