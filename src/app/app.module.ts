cd5ecd85861e8084b1fa53257d61a6894ccb7616
sanghavi-kartigayan <sanghavi.kartigayan@performance.ca>
2019-11-01 12:09:30 -0400

    Revert "directive added for dropdown click"
    This reverts commit 89dfb695e11a7089cd3d1c08b3c69319fbecaa65.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,8 +20,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
